bd63834a5324c04159eb71c274186f330473ca11
Brian Spendolini <69256666+JetterMcTedder@users.noreply.github.com>
2023-05-18 11:14:25 -1000

    Update install-dev-tools.sh

--- scripts/install-dev-tools.sh
+++ scripts/install-dev-tools.sh
@@ -1,10 +1,10 @@
-dotnet tool install -g microsoft.sqlpackage
-dotnet new -i Microsoft.Build.Sql.Templates
 sudo cp ./scripts/ms-repo.pref /etc/apt/preferences.d/
 sudo apt-get update
 sudo apt-get install azure-functions-core-tools-4
 sudo apt install dotnet-sdk-6.0 -y
 npm install -g @azure/static-web-apps-cli
+dotnet tool install -g microsoft.sqlpackage
+dotnet new -i Microsoft.Build.Sql.Templates
 dotnet tool install --global Microsoft.DataApiBuilder
 sudo apt-get update
 sudo apt-get install sqlcmd
