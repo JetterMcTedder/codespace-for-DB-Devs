0de8580aaf1ee635ca058188f7336ddbf554e1c6
Brian Spendolini <69256666+JetterMcTedder@users.noreply.github.com>
2023-06-08 12:11:20 -1000

    Update install-dev-tools.sh

--- scripts/install-dev-tools.sh
+++ scripts/install-dev-tools.sh
@@ -2,18 +2,20 @@ sudo cp ./scripts/ms-repo.pref /etc/apt/preferences.d/
 sudo apt-get update
 sudo apt-get install azure-functions-core-tools-4
 sudo apt install dotnet-sdk-6.0 -y
-sudo npm install -g @azure/static-web-apps-cli
+npm install -g @azure/static-web-apps-cli
 dotnet tool install -g microsoft.sqlpackage
 dotnet new -i Microsoft.Build.Sql.Templates
 dotnet tool install --global Microsoft.DataApiBuilder
 sudo apt-get update
 sudo apt-get install sqlcmd
 echo 'PATH=$PATH:$HOME/.dotnet/tools' >> ~/.bashrc
-dab --version 
-func version 
-dotnet --version
 echo "rm packages-microsoft-prod.deb"
 rm packages-microsoft-prod.deb
 echo "rm microsoft.gpg"
 rm microsoft.gpg
 ls
+mkdir -p /home/vscode/.swa/dataApiBuilder/0.7.6/
+cd /home/vscode/.swa/dataApiBuilder/0.7.6/
+wget https://github.com/Azure/data-api-builder/releases/download/v0.7.6/dab_linux-x64-0.7.6.zip@0.7.6
+unzip dab_linux-x64-0.7.6.zip
+chmod 777 *
